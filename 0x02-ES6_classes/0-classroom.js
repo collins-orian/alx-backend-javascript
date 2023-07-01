c2f0a3b1357ff3561149ef2fe3b50ca06dea03af
collins-orian <inaboyacollins@icloud.com>
2023-07-01 01:27:11 +0100

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,5 @@
 export default class ClassRoom {
-  _maxStudentsSize = 10;
-  constructor(name){
-    this.name = name
+  constructor(maxStudentsSize){
+    this._maxStudentsSize = maxStudentsSize
   }
 }
