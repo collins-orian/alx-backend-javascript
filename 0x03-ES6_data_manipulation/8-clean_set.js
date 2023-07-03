f2b48772e3cf1c224a35e505ed691ca1a9c4b3ca
collins-orian <inaboyacollins@icloud.com>
2023-07-03 23:03:55 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -12,12 +12,12 @@ export default function cleanSet(set, startString) {
     !set ||
     !startString ||
     !(set instanceof Set) ||
-    typeof startString !== "string"
+    typeof startString !== 'string'
   ) {
-    return "";
+    return '';
   }
   for (const value of set.values()) {
-    if (typeof value === "string" && value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       const valueSubStr = value.substring(startString.length);
 
       if (valueSubStr && valueSubStr !== value) {
@@ -25,5 +25,5 @@ export default function cleanSet(set, startString) {
       }
     }
   }
-  return parts.join("-");
+  return parts.join('-');
 }
